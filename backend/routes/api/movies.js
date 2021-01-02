c0027c460a166785c82d898af288f1e823dce036
jimmy <demeatricej.sherrod@gmail.com>
2021-01-02 04:52:52 -0600

    added backend route to specific books

--- backend/routes/api/movies.js
+++ backend/routes/api/movies.js
@@ -2,13 +2,34 @@ const express = require('express');
 const asyncHandler = require('express-async-handler');
 const { Movie } = require('../../db/models');
 
-
 const router = express.Router();
+
+function changeParamsToUseableId(req) {
+    //Looking for a dynamic way to query the database. I think this works and it's hilarious cuz it seems ridiculous and inefficient.
+    const id = req.url.split('/').join('');
+    // title = id.split('-').join(' '); LOL
+
+    return id
+}
 
 router.get('/', asyncHandler(async (req, res) => {
     const movies = await Movie.findAll();
     return res.json(movies);
 }));
+
+router.get('/:id', asyncHandler(async (req, res) => {
+
+    const id = changeParamsToUseableId(req);
+
+    const movie = await Movie.findOne({
+        where: {
+            id: id
+        }
+    })
+    
+    return res.json(movie);
+}));
+
 
 
 
