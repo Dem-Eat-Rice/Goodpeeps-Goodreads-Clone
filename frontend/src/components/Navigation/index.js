9c0e2e4cd14f6de2dfa55a90672caf4d88c485c6
jimmy <demeatricej.sherrod@gmail.com>
2021-01-10 13:50:46 -0600

    added some pictures, navbar resembles goodreads

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -5,6 +5,7 @@ import ProfileButton from './ProfileButton';
 import LoginForm from '../LoginFormPage/LoginForm';
 import './Navigation.css';
 import logo from "../../goodpeeps.png"
+import magnifyingGlass from "../../my.png"
 
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
@@ -12,13 +13,25 @@ function Navigation({ isLoaded }){
   let sessionLinks;
   if (sessionUser) {
     sessionLinks = (
-      <ProfileButton user={sessionUser} />
+      <>
+        <NavLink exact to="/">Home</NavLink>
+        <NavLink exact to="/shelves">My Movies</NavLink>
+        <NavLink exact to="/">Browse</NavLink>
+        <NavLink exact to="/">Community</NavLink>
+        <div class="searchBar">
+        <input className="search" type="text" placeholder="Search movies" />
+        <NavLink to="/nowhere">
+          <img id="glass" src={magnifyingGlass}/>
+        </NavLink>
+        </div>
+        <ProfileButton user={sessionUser} />
+      </>
       
     );
   } else {
     sessionLinks = (
       <>
-        <NavLink to="/signup">Sign Up</NavLink>
+        {/* <NavLink to="/signup">Sign Up</NavLink> */}
         <LoginForm />
       </>
     );
@@ -30,9 +43,9 @@ function Navigation({ isLoaded }){
         <NavLink exact to="/">
           <img src={logo}/>
         </NavLink>
-        <NavLink exact to="/">Home</NavLink>
         {isLoaded && sessionLinks}
       </ul>
+      <hr></hr>
     </>
   );
 }
