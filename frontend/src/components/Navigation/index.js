cefc0e2471212f79a4eab9b7a74db1db2136ce7a
jimmy <demeatricej.sherrod@gmail.com>
2021-01-10 07:44:20 -0600

    almost removed modal successfully

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-import LoginFormModal from '../LoginFormModal';
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
@@ -17,7 +16,7 @@ function Navigation({ isLoaded }){
   } else {
     sessionLinks = (
       <>
-        <LoginFormModal />
+        <NavLink to="/login">Log in</NavLink>
         <NavLink to="/signup">Sign Up</NavLink>
       </>
     );
